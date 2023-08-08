59e51b5c078b0995d835337f1e3ae5bc1428c785
Henrique Cecconi <58144622+HenriqueCecconi@users.noreply.github.com>
2023-08-08 00:04:30 -0300

    Refactor using methods to make adding shapes easy
    Here we use structs and methods so each shape have it's own methods to
    deal with calculating the Area and Perimeter. This way, we make adding
    new shapes easy, and each shape has it's own methods so we don't have
    to make a function deal with different structs and formulas to calculate
    each area/perimeter.

--- structs_method_interfaces/shapes.go
+++ structs_method_interfaces/shapes.go
@@ -1,14 +1,27 @@
 package structsmethodinterfaces
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (r Rectangle) Perimeter() float64 {
+	return 2*r.Height + 2*r.Width
+}
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+type Circle struct {
+	Radius float64
 }
--- structs_method_interfaces/shapes_test.go
+++ structs_method_interfaces/shapes_test.go
@@ -3,21 +3,43 @@ package structsmethodinterfaces
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	want := 40.0
+	t.Run("rectangles perimeter", func(t *testing.T) {
+		rectangle := Rectangle{20.0, 10.0}
+		got := rectangle.Perimeter()
+		want := 60.0
 
-	if got != want {
-		t.Errorf("Got %.2f, want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("Got %g, want %g", got, want)
+		}
+	})
+	t.Run("circles perimeter", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Perimeter()
+		want := 62.83185307179586
+
+		if got != want {
+			t.Errorf("Got %g, want %g", got, want)
+		}
+	})
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{20.0, 10.0}
-	got := Area(rectangle)
-	want := 200.0
+	t.Run("rectangles area", func(t *testing.T) {
+		rectangle := Rectangle{20.0, 10.0}
+		got := rectangle.Area()
+		want := 200.0
 
-	if got != want {
-		t.Errorf("Got %.2f, want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("Got %g, want %g", got, want)
+		}
+	})
+	t.Run("circles area", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("Got %g, want %g", got, want)
+		}
+	})
 }
