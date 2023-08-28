a431d32de62f1a9d447b940b5b81c67d3522a205
Henrique Cecconi <58144622+HenriqueCecconi@users.noreply.github.com>
2023-08-28 10:25:39 -0300

    Refactor all tests
    Make all tests table driven. Improving the slice of anonymous structs
    format and naming for clarity. Adding the t.Run inside the test to make
    sure our output is helpful

--- structs_method_interfaces/shapes_test.go
+++ structs_method_interfaces/shapes_test.go
@@ -2,45 +2,30 @@ package structsmethodinterfaces
 
 import "testing"
 
-func TestPerimeter(t *testing.T) {
-	checkPerimeter := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		perimeter := shape.Perimeter()
-		if perimeter != want {
-			t.Errorf("Got %g, want %g", perimeter, want)
-		}
-	}
-	t.Run("rectangles perimeter", func(t *testing.T) {
-		rectangle := Rectangle{20.0, 10.0}
-		want := 60.0
-		checkPerimeter(t, rectangle, want)
-	})
-	t.Run("circles perimeter", func(t *testing.T) {
-		circle := Circle{10.0}
-		want := 62.83185307179586
-		checkPerimeter(t, circle, want)
-	})
-	t.Run("triangles perimeter", func(t *testing.T) {
-		triangle := Triangle{12.0, 6}
-		want := 28.97056274847714
-		checkPerimeter(t, triangle, want)
-	})
-}
-
-func TestArea(t *testing.T) {
-	areaTests := []struct {
-		shape Shape
-		want  float64
+func TestShape(t *testing.T) {
+	shapeTests := []struct {
+		name         string
+		shape        Shape
+		hasArea      float64
+		hasPerimeter float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10.0}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0, hasPerimeter: 36.0},
+		{name: "Circle", shape: Circle{10.0}, hasArea: 314.1592653589793, hasPerimeter: 62.83185307179586},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0, hasPerimeter: 28.97056274847714},
 	}
 
-	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("Got %g want %g", got, tt.want)
-		}
+	for _, tt := range shapeTests {
+		t.Run(tt.name+" area", func(t *testing.T) {
+			areaGot := tt.shape.Area()
+			if areaGot != tt.hasArea {
+				t.Errorf("%#v area got: %g, area wanted: %g", tt.shape, areaGot, tt.hasArea)
+			}
+		})
+		t.Run(tt.name+" perimeter", func(t *testing.T) {
+			perimeterGot := tt.shape.Perimeter()
+			if perimeterGot != tt.hasPerimeter {
+				t.Errorf("%#v perimeter got: %g, perimeter wanted: %g", tt.shape, perimeterGot, tt.hasPerimeter)
+			}
+		})
 	}
 }
